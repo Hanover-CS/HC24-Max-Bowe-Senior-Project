edc4efad9b5d7f0f0897cd5d74042d7f88b9e348
Max Bowe <bowem24@hanover.edu>
2023-10-19 13:45:17 -0400

    added basic character with basic movement and a collidable object

--- project/Characters/Player/player.gd
+++ project/Characters/Player/player.gd
@@ -1,9 +1,13 @@
-extends Node2D
-
-var planet_map = [{"type": "fight", "explored": false}, 
-					{"type": "boss", "explored": false}, 
-					{"type": "fight", "explored": false}, 
-					{"type": "mystery", "explored": false}]
+extends CharacterBody2D
 
 
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
 
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction * 500
+	move_and_slide()
