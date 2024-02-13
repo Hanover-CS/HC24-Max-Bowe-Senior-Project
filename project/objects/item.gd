5ff6d5a0aa6ed4dc5f9b9d8ca39d8d3ec48a3a72
Max Bowe <bowem24@hanover.edu>
2024-02-13 12:21:40 -0500

    upgrade list appears when touching item

--- project/objects/item.gd
+++ project/objects/item.gd
@@ -7,6 +7,13 @@ signal itemSelected
 func _on_item_list_item_selected(index):
 	if index == 0:
 		global.playerSpeed += 200
-	else: 
+	elif index == 1: 
 		global.playerDamage += 10
+	else:
+		global.playerHealth += 10
 	emit_signal("itemSelected")
+
+# makes options visible when player touches item object
+
+func _on_area_2d_area_entered(area):
+	$ItemList.visible = true
